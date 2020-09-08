9848cd882668786842713ec60ac1b791493a95f0
Developer <Developer>
2020-09-08 17:32:30 +0530

    changes for html

--- dist/js/datatable.script.js
+++ dist/js/datatable.script.js
@@ -1,56 +1,57 @@
 (function ($) {
-  "use strict";
-  var editor;
-  $("#example").DataTable({
-    dom: "Bfrtip",
-    buttons: ["copy", "csv", "excel", "pdf", "print"],
-    responsive: true,
-  });
+    "use strict";
+    var editor;
+    $("#example").DataTable({
+        dom: "Bfrtip",
+        buttons: ["copy", "csv", "excel", "pdf", "print"],
+        responsive: true,
+    });
 
-  $("#firmSearch").DataTable({
-    responsive: true,
-    searching: false,
-    lengthChange: false,
-  });
+    $("#firmSearch").DataTable({
+        responsive: true,
+        searching: false,
+        lengthChange: false,
+    });
 
-  $("#searchresult").DataTable({
-    responsive: true,
-    searching: false,
-    lengthChange: false,
-    colReorder: {
-      order: [4, 3, 2, 1, 0, 5],
-    },
-  });
+    $("#searchresult").DataTable({
+        responsive: true,
+        searching: false,
+        lengthChange: false,
+        colReorder: {
+            order: [4, 3, 2, 1, 0, 5],
+        },
+    });
 
-  $("#requestserviceresult").DataTable({
-    responsive: true,
-    searching: false,
-    lengthChange: false,
-  });
+    $("#requestserviceresult").DataTable({
+        responsive: true,
+        searching: false,
+        lengthChange: false,
+    });
 
-  $("#companytable").DataTable({
-    responsive: true,
-    searching: false,
-    lengthChange: false,
-    dom: "Bfrtip",
-    buttons: ["csv", "pdf"],
-  });
+    $("#companytable").DataTable({
+        responsive: true,
+        searching: false,
+        lengthChange: false,
+        dom: "Bfrtip",
+        buttons: ["csv", "pdf"],
+        colReorder: true
+    });
 
-  $("#manageInvoice").DataTable({
-    responsive: true,
-    searching: false,
-    lengthChange: false,
-  });
+    $("#manageInvoice").DataTable({
+        responsive: true,
+        searching: false,
+        lengthChange: false,
+    });
 
-  $("#managemylogin").DataTable({
-    responsive: true,
-    searching: false,
-    lengthChange: false,
-  });
+    $("#managemylogin").DataTable({
+        responsive: true,
+        searching: false,
+        lengthChange: false,
+    });
 
-  $("#paymentreport").DataTable({
-    responsive: true,
-    searching: false,
-    lengthChange: false,
-  });
+    $("#paymentreport").DataTable({
+        responsive: true,
+        searching: false,
+        lengthChange: false,
+    });
 })(jQuery);
